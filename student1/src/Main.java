5a63a38756a7de76c49f2bfaaaa92a04a1e33509
emariec <79215468+emariec@users.noreply.github.com>
2021-04-22 13:34:15 +0200

    main added

--- student1/src/Main.java
+++ student1/src/Main.java
@@ -1,2 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception { //throws Exception
+        /*String url="jdbc:sqlite:/Users/esthermariechristensen/Documents/Softwaredevelopment/Trains.db";
+        TrainModel TDB=new TrainModel(url);
+        TrainController control=new TrainController(TDB);*/
+        //StudentView view= new StudentView(); //(control)
+        //  control.setView(view);
+        primaryStage.setTitle("Student- and course information");
+        //primaryStage.setScene(new Scene(view.asParent(),600,475));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
