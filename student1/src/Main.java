594b3127914d6eab4fb27587763ad24daf4c4a27
ClaraMcNair <79084200+ClaraMcNair@users.noreply.github.com>
2021-04-28 11:41:52 +0200

    kommentarer main og view

--- student1/src/Main.java
+++ student1/src/Main.java
@@ -1,18 +1,20 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//hej
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception { //throws Exception
+    public void start(Stage primaryStage) throws Exception {
+        // Connect to the database "identifier.sqlite"
+        // and initialize classes
         String url = "jdbc:sqlite:student1/identifier.sqlite";
         StudentModel SDB = new StudentModel(url);
         Controller control = new Controller(SDB);
         StudentView view = new StudentView(control);
         control.setView(view);
 
+        // Initialize and show a stage with a new scene
         primaryStage.setTitle("Student- and course information");
         primaryStage.setScene(new Scene(view.asParent(),600,475));
         primaryStage.show();
--- student1/src/StudentView.java
+++ student1/src/StudentView.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.GridPane;
 public class StudentView {
     private Controller control;
     private GridPane Startview;
+
+    // Creates all the different buttons, labels, combo boxes and the text area
     Button exitBtn=new Button("Exit");
     Button StudentInfoBtn= new Button("Get student's info");
     Button CourseInfoBtn= new Button("Get course's info");
@@ -25,42 +27,38 @@ public class StudentView {
     ComboBox<Integer> GradeComB=new ComboBox<>();
 
 
-    public StudentView(Controller control){ // StudentView(StudentController control)
+    public StudentView(Controller control){
         this.control=control;
         createAndConfigure();
     }
 
     private void createAndConfigure(){
+        // Creates a new GridPane and sets the layout
         Startview=new GridPane();
         Startview.setMinSize(300, 200);
         Startview.setPadding(new Insets(10,10,10,10));
         Startview.setVgap(5);
         Startview.setHgap(1);
+
+        // Inserts all the elements at specified locations in the GridPane StartView
         Startview.add(exitBtn,20,20);
         Startview.add(StudentInfoBtn,1,6);
         Startview.add(CourseInfoBtn,1,7);
         Startview.add(GradeBtn,1,8);
+
         Startview.add(StudentLbl,1,1);
         Startview.add(CourseLbl,1,3);
         Startview.add(GradeLbl, 1, 5);
+
         Startview.add(InfoText,1,9,15,9);
 
         Startview.add(StudentComB, 15,1 );
         Startview.add(CourseComB,15,3);
         Startview.add(GradeComB,15,5);
 
-
-        /*ObservableList<String> stations=control.getStations();
-        StartStationComB.setItems(stations);
-        EndStationComB.setItems(stations);
-        StartStationComB.getSelectionModel().selectFirst();
-        EndStationComB.getSelectionModel().selectFirst();
-        HourComB.setItems(control.getHours());
-        HourComB.getSelectionModel().selectFirst();
-        MinuteComB.setItems(control.getMinutes());
-        MinuteComB.getSelectionModel().selectFirst();*/
-        ObservableList<String> students=control.getStudent();
-        StudentComB.setItems(students);
+        // Set items for the three different combo boxes
+        // and sets the first shown/selected item
+        StudentComB.setItems(control.getStudent());
         StudentComB.getSelectionModel().selectFirst();
         CourseComB.setItems(control.getCourses());
         CourseComB.getSelectionModel().selectFirst();
